271d5255fb1a7020a8188763acb55b6d7e6e0e09
RazvanFilote <razvan.filote@stud.ubbcluj.ro>
2026-03-30 21:56:11 +0300

    Fix PropertyChanged and ContentDialog await errors

--- matchmaking/ViewModels/ObservableObject.cs
+++ matchmaking/ViewModels/ObservableObject.cs
@@ -7,6 +7,9 @@ public abstract class ObservableObject : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
     protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
         if (Equals(field, value))
